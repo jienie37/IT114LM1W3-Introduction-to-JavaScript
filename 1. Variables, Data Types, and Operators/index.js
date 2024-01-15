24aab652168cc7eb5c907d7061272455d5661446
jin <jiengreycrisostomo@gmail.com>
2024-01-15 19:20:48 +0800

    act

--- 1. Variables, Data Types, and Operators/index.js
+++ 1. Variables, Data Types, and Operators/index.js
@@ -2,46 +2,63 @@
     Resources:
     - MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
 */
-
 // TODO 1.1 Use the var keyword to define a variable. Log the variable to the console. Now try moving the variable declaration after you log the variable.
 // Your code here
+var f = 5;
+var e = 10;
 
 // TODO 1.2 Use the let keyword to define a variable.
 // Your code here
+let c = 10 + 12;
+let b = 16 - c;
 
 // TODO 1.3 Use the const keyword to define a variable. Try reassigning the value of the variable. What happens?
 // Your code here
+const a = 75 - b;
+const d = 5 + a;
 
 // Checkpoint 1.1 What is the difference between var, let, and const?
-// Answer: 
+// Answer: the answer would be the scope of things they use
+
 
 // TODO 1.4 Try out the different arithmetic operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#arithmetic_operators
 // Your code here
+const answer = a + d - e * 100 / 10;
 
 // Checkpoint 1.2 What operators did you use?
-// Answer: 
-// Your code here
+// Answer: + - / *
+
 
 // TODO 1.5 Try out the different string operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#string_operators
 // Your code here
+console.log("it114l a54 " + "Jien " + `answer is ${answer}`);
+
+
 
 // Checkpoint 1.3 What operators did you use?
-// Answer: 
+// Answer: +
 
 // TODO 1.6 Try out the different logical operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#logical_operators
 // Your code here
-
+const a2 = a > 5 && b > 10;
+const a1 = b <= 7 || c >= 16;
+console.log(`${a1} and ${a2}`);
 // Checkpoint 1.4 What operators did you use?
-// Answer: 
+// Answer: > < >= <=
 
 
 // TODO 1.7 Try out the different relational operators
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#relational_operators
-
+const classs = ["teacher", "student", "chair", "dean", "professor"];
+0 in classs; 
+3 in classs;
+6 in classs;
+"man" in classs;
 
 // Checkpoint 1.5 What does [] == false evaluate to? Why does that happen? Research why
-// Answer: 
+// Answer: empty brackets are considered as false
 // Your code here
+console.log([] == false);
--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -3,61 +3,115 @@
     - MDN Docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Control_flow_and_error_handling
 */
 
-const randomNumber = Math.floor((Math.random() * 100) + 1); // feel free to mock this value for testing
+const randomNumber = 21; // feel free to mock this value for testing
+console.log(randomNumber);
 
 // TODO 2.1 Display "more than fifty" if randomNumber is more than fify
 // Your code here
+if (randomNumber>50){
+    console.log("more than fifty");
+}
+else {
+    console.log("no");
+}
 
 // TODO 2.2 Display whether the random number is odd or even
 // Your code here
+if (randomNumber % 2 == 0){
+    console.log("EVEN");
+}
+else {
+    console.log("Odd");
+}
 
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
 // Your code here
+if (randomNumber % 5 == 0 && randomNumber % 3 == 0){
+    console.log("fizzbuzz")
+}
+else if(randomNumber % 5 == 0){
+    console.log("buzz");
+}
+else if(randomNumber % 3 == 0){
+    console.log("fizz");
+}
+else{
+    console.log(randomNumber)
+}
 
-let toDisplay = ""
+
+let toDisplay = (randomNumber % 2 == 0) ? 'Even' : 'Odd'
 // TODO 2.4 Use the ternary conditional operator to set toDisplay to "Even" if randomNumber is even and "Odd" if randomNumber is odd
 // Your code here
 console.log("toDisplay", toDisplay);
 
 // Checkpoint 2.1 How do you use switch statements and when would you use them? Try researching the answer
-// Answer: 
+// Answer: to test the equality of a variable against several values specified in the test cases
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
-const n = 10
+const n = 10;
 // Your code here
+for (let d = 0; d < n; d++){
+    console.log(d);
+}
+
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
 // Your code here
+i=0
+while (i < list.length){
+    console.log(list[i]);
+    i++
+}
 
 // Checkpoint 2.3 What is the difference between do while and while loop?
-// Answer: 
+// Answer: do while does the "do" syntax first before looping
 
 // TODO 2.7 Use a for of loop to display all the values in the list
 // Your code here
+for (const b of list) {
+    console.log(b+"1")
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
 // Your code here
+for (const a in list) {
+    if (Object.hasOwnProperty.call(list, a)) {
+        const element = list[a];
+        console.log(element+"2");
+    }
+}
 
 // TODO 2.9 Use the for each method of the list to display all its values 
 // Your code here
+list.forEach(element => {
+    console.log(element+"3");
+});
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+/* Answer: Foreach = makes you list everything in the given list
+For in 
+
+*/
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
 const numerator = Math.floor((Math.random() * 100) + 1);
-const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
+const denominator = 0; // feel free to mock this value for testing
 
-
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
+try {
+    if  (denominator === 0){
+        throw new Error("Division by zero error")
+    } else {
+        console.log(numerator / denominator);
+    }
 }
-
-
-
+catch{
+    console.log("Division by zero");
+}
+finally{
+    console.log("Cleaning up resources");
+}
