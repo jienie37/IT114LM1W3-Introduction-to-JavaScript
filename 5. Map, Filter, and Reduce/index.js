81b89692f4b36efaf70421463279362bba57e6b4
jienie37 <150808923+jienie37@users.noreply.github.com>
2024-01-16 21:24:54 +0800

    act

--- 5. Map, Filter, and Reduce/index.js
+++ 5. Map, Filter, and Reduce/index.js
@@ -17,12 +17,12 @@ console.log("Sum: ", sum);
 
 // TODO 5.4 Use the map method to convert all elements in the array to uppercase
 const words = ['hello', 'world'];
-const uppercaseWords = words.map(word => word.toUpperCase());
-console.log("Uppercase Words: ", uppercaseWords);
+const bigLetter = words.map(word => word.toUpperCase());
+console.log("Uppercase Words: ", bigLetter);
 
 // TODO 5.5 Use the filter method to create a new array that contains only the words with more than four letters in the array
-const wordsWithMoreThanFourLetters = words.filter(word => word.length > 4);
-console.log("Words with More Than Four Letters: ", wordsWithMoreThanFourLetters);
+const fourLetterWord = words.filter(word => word.length > 4);
+console.log("Words with More Than Four Letters: ", fourLetterWord);
 
 // TODO 5.6 Use the reduce method to concatenate all words in the array into a single string
 const concatenatedString = words.reduce((accumulator, word) => accumulator + ' ' + word, '');
@@ -30,6 +30,6 @@ console.log("Concatenated String: ", concatenatedString.trim());
 
 // Checkpoint 5.1 Summarize what map, filter, and reduce do
 // Answer: 
-// - map: creates a new array by applying a provided function to each element of the original array.
-// - filter: creates a new array containing only the elements that satisfy a specified condition.
-// - reduce: applies a function to each element of the array, reducing it to a single value. It takes an accumulator and the current element as parameters.
+// map = creates a new array by applying a function to the elemnts
+// filter = creates a new array elements that has a specific condition
+// reduce = applies a function to each element of the array, reducing it to a single value. It takes an accumulator and the current element as parameters
